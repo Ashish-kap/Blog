191fedc94052e496da2d492aa449582eedab4888
Ashish kap <ashishkap157@gmail.com>
2023-01-04 02:48:57 +0530

    minor changes

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const app = require('./app.js')
 const mongoose = require('mongoose')
+const cors=require("cors");
 
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) 
 
 mongoose.connect('mongodb+srv://srigbok:test1234@cluster0.oj5qw.mongodb.net/myblog',{
  
